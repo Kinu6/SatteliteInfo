7ec7f42dc4cb475b66413b6eb93b6590c2b54a41
kravi <kravi@Keshri>
2024-02-03 09:46:22 +0530

    commit 3/02/946

--- src/test/java/satteliteInfo/ContactTests/TC_01Test.java
+++ src/test/java/satteliteInfo/ContactTests/TC_01Test.java
@@ -25,7 +25,7 @@ public class TC_01Test extends BaseClass {
         
 		Thread.sleep(2000);
 		
-		Reporter.log("Naviated to contacts link");
+		Reporter.log("Naviated tocontacts link");
 		ContactsPage ccp = new ContactsPage(driver);
 		wUtil.waitForElementToBeVisisble(driver, ccp.getCreateNewIcon());;
 		wUtil.waitForElementToBeClickabale(driver, ccp.getCreateNewIcon());
